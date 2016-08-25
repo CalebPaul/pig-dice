0303d70081725f38f8766e3fcc29a7ccb21ad862
Caleb Paul and David Ayala <david@>
2016-08-25 08:25:12 -0700

    add basic scoring for computer player in js, and print score to html

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,15 @@
 var userDieResult = 0;
 var userTurnResult = 0;
 var userTotalScore = 0;
-
+var compDieResult = 0;
+var compTurnResult = 0;
+var compTotalScore = 0;
 function User(totalScore, turnScore, dieScore, endgame ) {
   this.totalScore = totalScore;
   this.turnScore = turnScore;
   this.dieScore = dieScore;
   this.endgame = endgame;
 }
-
-function Comp(totalScore, turnScore, dieScore, endgame ) {
-  this.totalScore = totalScore;
-  this.turnScore = turnScore;
-  this.dieScore = dieScore;
-  this.endgame = endgame;
-}
-//prototypes - rng, CompLogic, hold, rolled1
 User.prototype.dieroll = function(){
   userDieResult = parseInt(Math.floor(Math.random() * 6) + 1);
     if (userDieResult === 1) {
@@ -25,7 +19,6 @@ User.prototype.dieroll = function(){
     }
   return userDieResult;
 }
-
 User.prototype.addRoll = function(){
   if (userDieResult === 1) {
       userDieResult = 0;
@@ -33,15 +26,40 @@ User.prototype.addRoll = function(){
   userTurnResult += userDieResult;
   return userTurnResult;
 }
-
 User.prototype.total = function(){
   userTotalScore += userTurnResult;
   return userTotalScore;
+}
+function Comp(totalScore, turnScore, dieScore, endgame ) {
+  this.totalScore = totalScore;
+  this.turnScore = turnScore;
+  this.dieScore = dieScore;
+  this.endgame = endgame;
+}
+Comp.prototype.dieroll = function(){
+  compDieResult = parseInt(Math.floor(Math.random() * 6) + 1);
+    if (compDieResult === 1) {
+      compDieResult = 1;
+      compTurnResult = 0;
+    }
+  return compDieResult;
+}
+Comp.prototype.addRoll = function(){
+  if (compDieResult === 1) {
+      compDieResult = 0;
+    }
+  compTurnResult += compDieResult;
+  return compTurnResult;
+}
+Comp.prototype.total = function(){
+  compTotalScore += compTurnResult;
+  return compTotalScore;
 }
 //User Logic
 $(document).ready(function() {
   //event.preventDefault();
   var player1 = new User();
+  var comp1 = new Comp();
 
     $("#user-roll").click(function() {
       player1.dieScore = player1.dieroll();
@@ -52,6 +70,11 @@ $(document).ready(function() {
     $("#hold-roll").click(function() {
       player1.totalScore = player1.total();
       $("#totalScore").text(player1.totalScore);
+      //computer's turn
+      comp1.dieScore = comp1.dieroll();
+      comp1.turnScore = comp1.addRoll();
+      $("#compDieScore").text(comp1.dieScore);
+      $("#compTurnScore").text(comp1.turnScore);
     });
 
 });
